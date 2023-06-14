851910e8c45040d5363c4d8d5ef0026d9e24a848
Evgeniy Ivanov <i@eivanov.com>
2023-06-14 20:46:13 +0200

    Fix a glitch with number of sessions

--- src/main/java/com/oltpbenchmark/DBWorkload.java
+++ src/main/java/com/oltpbenchmark/DBWorkload.java
@@ -160,14 +160,27 @@ public class DBWorkload {
                 wrkld.setLoaderThreads(loaderThreads);
             }
 
+            int minSessions = wrkld.getMinSessions();
+            int maxSessions = wrkld.getMaxSessions();
+
             if (xmlConfig.containsKey("minSessions")) {
-                int minSessions = xmlConfig.getInt("minSessions");
+                minSessions = xmlConfig.getInt("minSessions");
                 wrkld.setMinSessions(minSessions);
+
+                if (minSessions > maxSessions) {
+                    maxSessions = minSessions;
+                    wrkld.setMaxSessions(maxSessions);
+                }
             }
 
             if (xmlConfig.containsKey("maxSessions")) {
-                int maxSessions = xmlConfig.getInt("maxSessions");
+                maxSessions = xmlConfig.getInt("maxSessions");
                 wrkld.setMaxSessions(maxSessions);
+
+                if (maxSessions < minSessions) {
+                    minSessions = maxSessions;
+                    wrkld.setMinSessions(minSessions);
+                }
             }
 
             String isolationMode = xmlConfig.getString("isolation[not(@bench)]", "TRANSACTION_SERIALIZABLE");
